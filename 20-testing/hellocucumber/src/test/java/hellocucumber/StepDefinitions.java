df33a753331ac213c4b7163c910d7a653200a56f
erik1110 <oceanuheart@gmail.com>
2025-03-28 16:32:14 +0000

    feat: add cucumber

---
+++ 20-testing/hellocucumber/src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,47 @@
+package hellocucumber;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+class IsItFriday {
+    static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
+
+public class StepDefinitions {
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertThat(actualAnswer).isEqualTo(expectedAnswer);
+    }
+
+}
